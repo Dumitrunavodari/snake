17a27c4dabb5e78dc4e733904af5cd837ad8b378
Dumitrunavodari <75925413+Dumitrunavodari@users.noreply.github.com>
2022-02-19 07:33:21 +0200

    Add files via upload

--- js/generateApple.js
+++ js/generateApple.js
@@ -1,11 +1,16 @@
 function generateApple() {
-  var appleRowPosition = Math.floor(Math.random() * 20 + 1);
-  var appleColumnPosition = Math.floor(Math.random() * 20 + 1);
-  var applePosition = appleRowPosition +'_'+ appleColumnPosition;
-  var appleSrc = document.getElementById(applePosition).src;
-  if(1 * appleSrc[appleSrc.length - 13] == 1) {
-    generateApple();
+  var rightApple = 0;
+  var applePosition;
+  while (!rightApple) {
+    var appleRowPosition = Math.floor(Math.random() * 20 + 1);
+    var appleColumnPosition = Math.floor(Math.random() * 20 + 1);
+    applePosition = appleRowPosition +'_'+ appleColumnPosition;
+    var appleSrc = document.getElementById(applePosition).src;
+    if(1 * appleSrc[appleSrc.length - 13] != 1) {
+      rightApple = 1;
+    }
   }
-  document.getElementById(applePosition).src = "pics/2_0_00_00.png";
+
+  document.getElementById(applePosition).src = "pics/2_0_00_00.png";    
   return applePosition;
 }
